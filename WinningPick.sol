1df468aed19771f120c4acb2a815de34328290c5
Dracogray24 <grimmdrk@gmail.com>
2024-11-19 16:27:27 +0800

    new

--- WinningPick.sol
+++ WinningPick.sol
@@ -4,12 +4,12 @@ pragma solidity 0.8.18;
 
 contract LotteryPick {
     function RequiredPick(uint _cash) public pure {
-        require(_cash > 100, "Input must be greater than 100");
+        require(_cash < 100, "Input must be lesser than 100");
     }
 
     function NumberPickRevert(uint _cash) public pure {
-        if (_cash <= 100) {
-            revert("Input must be greater than 100");
+        if (_cash >= 99) {
+            revert("Input must be lesser than 100");
         }
     }
 
